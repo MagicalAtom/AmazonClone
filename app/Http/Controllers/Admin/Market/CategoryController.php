8a8854a23114f3ae62287abc2f761b0e3d89d7fb
MagicalAtom <magicsoftware.org@gmail.com>
2023-04-24 07:56:04 -0700

    add category section to panel

--- app/Http/Controllers/Admin/Market/CategoryController.php
+++ app/Http/Controllers/Admin/Market/CategoryController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
-class CategoryController extends Controller
-{
+class CategoryController extends Controller{
     public function index()
     {
+        //
+    }
 
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
     }
 }
